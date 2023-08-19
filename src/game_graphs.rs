943ffa0713d8112b247d6525cd03d7f6805303e4
Raoul Luqué <raoulsluque@gmail.com>
2023-08-19 20:50:35 +0200

    Basic Structure of module

--- src/game_graphs.rs
+++
@@ -1,3 +0,0 @@
-pub mod node;
-pub mod edge;
-pub mod graph;
---
+++ src/graph.rs
@@ -0,0 +1,39 @@
+// Graph with generic type
+// Nodes are encoded as HashMap<Node<T>,U> U being a value for each node
+// Edges are encoded as HashSet<Tuple<Node,Node>>
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+
+
+pub struct Graph<T> {
+    /// Set of vertices in the graph
+    vertices: HashSet<T>,
+
+    /// Set of edges in the graph
+    edges: HashSet<(T, T)>,
+
+    /// Mapping of vertices to their labels
+    vertex_labels: HashMap<T, String>,
+
+    /// Mapping of vertices to vector of their inbound neighbours
+    inbound_table: HashMap<T, Vec<T>>,
+
+    /// Mapping of vertices to vector of their inbound neighbours
+    outbound_table: HashMap<T, Vec<T>>,
+}
+
+impl<T> Graph<T> {
+    /// Creates a new graph
+    pub fn new() -> Graph<T> {
+        Graph {
+            vertices: HashSet::new(),
+            edges: HashSet::new(),
+            vertex_labels: HashMap::new(),
+            inbound_table: HashMap::new(),
+            outbound_table: HashMap::new(),
+        }
+    }
+    
+
+}
